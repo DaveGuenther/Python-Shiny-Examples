79d5d119c25f2b682aa23b23f1706bb9c2e9ca19
Dave Guenther <dave.j.guenther.stuff@gmail.com>
2024-11-30 20:45:28 -0500

    Removed more 'song' references :D

--- pill_sidebar_to_accordion_responsive/pill_module.py
+++ pill_sidebar_to_accordion_responsive/pill_module.py
@@ -2,14 +2,14 @@ from shiny import ui, reactive, module
 
 @module.ui
 def module_ui(game_name):
-    return ui.input_action_button(id='btn_song', label=game_name).add_class('green').add_style('width:100%;'),
+    return ui.input_action_button(id='btn_game', label=game_name).add_class('green').add_style('width:100%;'),
 
 @module.server
 def module_server(input, output, session, game_id, selected_game):
 
     @reactive.effect
-    @reactive.event(input.btn_song)
-    def set_selected_song():
+    @reactive.event(input.btn_game)
+    def set_selected_game():
         selected_game.set(game_id)
 
 
